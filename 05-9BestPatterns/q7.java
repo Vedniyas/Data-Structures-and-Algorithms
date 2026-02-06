f68ba9f5e8fa3124f93492cc56dbf2dcc231576f
Vedniyas Vyas <vedniyasvyas@gmail.com>
2026-02-06 17:10:06 +0530

    Revising Codes

--- 05-9BestPatterns/q7.java
+++ 05-9BestPatterns/q7.java
@@ -5,10 +5,10 @@ public class q7 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Rows = ");
         int n = sc.nextInt();
-
+        
         for(int i = 1; i<=n; i++){
-            for(int j=1; j<=n-i+1; j++){
-                System.out.print(j);
+            for(int j = 1 ; j<=n-i+1; j++){
+                System.out.print(j + " ");
             }
             System.out.println();
         }
